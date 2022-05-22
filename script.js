1290c49e176f0d1981f86e85bad60b4a39ae6894
rtmdev <dreams200260@gmail.com>
2022-05-22 19:26:20 +0900

    Probressbar 오류 수정

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+let pb; //ProgressBar 객체가 담기는 변수
+
+//html DOM이 로드된 후 실행
+document.addEventListener('DOMContentLoaded',function(){
+    pb = new Progress(0, 0, 100, {parent : ".progress"});
+    console.log(pb);
+    checkTokenAndRun();
+
+});
+
+chrome.runtime.onMessage.addListener(
+    (request, sender, sendResponse) => {
+        //console.log('메시지 받음');
+
+        request = request.percent;
+        updatePercent(request[0], request[1]);
+        //sendResponse({ msg:"메시지 잘 받았으니 돌려드림" });
+
+    });
 
 function elt(type, prop, ...childrens) {
     let elem = document.createElement(type);
@@ -11,79 +30,59 @@ function elt(type, prop, ...childrens) {
   
 //Progress class
 class Progress {
-constructor(now, min, max, options) {
-  this.dom = elt("div", {
-    className: "progress-bar"
-  });
-  this.min = min;
-  this.max = max;
-  this.intervalCode = 0;
-  this.now = now;
-  this.syncState();
-  if(options.parent){
-    document.querySelector(options.parent).appendChild(this.dom);
-  }
-  else document.body.appendChild(this.dom)
-}
-
-syncState() {
-  this.dom.style.width = this.now + "%";
-}
-
-startTo(step, time) {
-  if (this.intervalCode !== 0) return;
-  this.intervalCode = setInterval(() => {
-    console.log("sss")
-    if (this.now + step > this.max) {
-      this.now = this.max;
-      this.syncState();
-      clearInterval(this.interval);
+    constructor(now, min, max, options) {
+      this.dom = elt("div", {
+        className: "progress-bar"
+      });
+      this.min = min;
+      this.max = max;
       this.intervalCode = 0;
-      return;
+      this.now = now;
+      this.syncState();
+      if(options.parent){
+        document.querySelector(options.parent).appendChild(this.dom);
+      }
+      else document.body.appendChild(this.dom)
     }
-    this.now += step;
-    this.syncState()
-  }, time)
+
+    syncState() {
+      this.dom.style.width = this.now + "%";
+    }
+
+    startTo(step, time) {
+      if (this.intervalCode !== 0) return;
+      this.intervalCode = setInterval(() => {
+        console.log("sss")
+        if (this.now + step > this.max) {
+          this.now = this.max;
+          this.syncState();
+          clearInterval(this.interval);
+          this.intervalCode = 0;
+          return;
+        }
+        this.now += step;
+        this.syncState()
+      }, time)
+    }
+
+    end() {
+      this.now = this.max;
+      clearInterval(this.intervalCode);
+      this.intervalCode = 0;
+      this.syncState();
+    }
 }
-end() {
-  this.now = this.max;
-  clearInterval(this.intervalCode);
-  this.intervalCode = 0;
-  this.syncState();
-}
-}
-  
-  
-let pb; //ProgressBar 객체가 담기는 변수
-
-//html DOM이 로드된 후 실행
-document.addEventListener('DOMContentLoaded',function(){
-    pb = new Progress(0, 0, 100, {parent : ".progress"});;
-    checkTokenAndRun();
-});
-
-chrome.runtime.onMessage.addListener(
-    (request, sender, sendResponse) => {
-        //console.log('메시지 받음');
-
-        request = request.percent;
-        updatePercent(request[0], request[1]);
-        //sendResponse({ msg:"메시지 잘 받았으니 돌려드림" });
-
-    });
 
 //퍼센트 업데이트
 function updatePercent(completedTasks, totalTasks)
 {
-    let spanObject = document.querySelector("#loading_percent");
-    if (spanObject !== null)
-    {
-        let percent = Math.round(completedTasks / totalTasks * 100);
-        pb.now = percent;
-        pb.syncState();
-        spanObject.innerHTML = String(percent);
-        //console.log(percent);
-    }
+
+    let percent = Math.round(completedTasks / totalTasks * 100);
+    pb.now = percent;
+    pb.syncState();
+    spanObject.innerHTML = String(percent);
+    //console.log(percent);
+
 }
 
 
@@ -534,8 +533,7 @@ function getComponents()
 
             RequestCompleteCount++;
             chrome.runtime.sendMessage({percent: [RequestCompleteCount,TargetCompleteFlag]}, (response) => {
-                console.log("메시지 콜백 받음.");
-                console.log(response);
+
             });
         }).fail(function(a, b,c,d)
         {
@@ -617,8 +615,7 @@ function getComponents()
             }
             RequestCompleteCount++;
             chrome.runtime.sendMessage({percent: [RequestCompleteCount,TargetCompleteFlag]}, (response) => {
-                console.log("메시지 콜백 받음.");
-                console.log(response);
+
             });
 
         }).fail(function(a, b,c,d)
